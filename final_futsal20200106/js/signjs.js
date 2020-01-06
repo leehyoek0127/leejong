760bbe6d5b1824977e673c853005233d403d22a4
leehyoek0127 <58802091+leehyoek0127@users.noreply.github.com>
2020-01-06 15:34:56 +0900

    ㅇㅇ

--- final_futsal20200106/js/signjs.js
+++ final_futsal20200106/js/signjs.js
@@ -32,16 +32,10 @@
 	var confirmid
 	function confirmId()
 	{	
-	var uid = join.id.value; 
-	var idReg = /^[a-zA-Z]+[a-z0-9A-Z]{7,13}$/g;
-
-	if( !idReg.test( uid ) ) {
-
-	alert("아이디를 확인해주세요. 아이디는 영문으로 시작하는 8~14자 사이의 아이디만 유효합니다.");
-
-	return;
-
-	}
+		if(join.id.value == ""){
+			alert("ID를 입력하세요.");
+			return;
+		}
 		url = "confirmId.jsp?id=" + join.id.value;
 		confirmid=join.id.value;
 		
@@ -60,63 +54,55 @@
 		var idlen=join.id.value.length
 		var pwlen=join.pw.value.length
 		var namelen=join.name.value.length
-		var hp2_len=join.hp2.value
-		var hp3_len=join.hp3.value
+		var hp2_len=join.hp2.value.length
+		var hp3_len=join.hp3.value.length
 		var em1_len=join.em1.value.length
 		var em2_len=join.em2.value.length
-		var hpReg2 = /^[0-9]+[0-9]{2,5}$/g;
-		var hpReg3 = /^[0-9]+[0-9]{2,5}$/g;
 		
 		
 
 		if ((idlen <8) || (idlen >14))
 		{
-			alert("아이디를 확인해주세요. 아이디는 영문으로 시작하는 8~14자 사이의 아이디만 유효합니다.")
+			alert("아이디를 확인하세요. 8~14자 사이의 아이디만 유효합니다.")
 			join.id.focus()
 			
 		}
 		else if(join.hidden.value!=1 || confirmid!=join.id.value){
-			alert("아이디 중복확인이 필요합니다.")
+			alert("아이디중복확인을 하세요.")
 		}
 
 		
 		else if ((pwlen <4) || (pwlen >14))
 		{
 
-			alert("비밀번호를 확인해주세요.비밀번호는 4~14자 사이의 비밀번호만 유효합니다.")
+			alert("비밀번호를 확인하세요. 4~14자 사이의 비밀번호만 유효합니다.")
 			join.pw.focus()
 		}
 
 		else if(join.pw.value!=join.pwch.value)
 		{
-			alert("비밀번호가 일치하지 않습니다.")
+			alert("비밀번호일치여부를 다시 확인하세요.")
 			join.pwch.focus()
 		}
 
 		
 		else if(namelen<=0)
 		{
-			alert("이름을 입력해주세요.")
+			alert("이름을 입력하세요.")
 			join.name.focus()
 		}
+
 		
-		
-		else if(!hpReg2.test(hp2_len))
+		else if(hp2_len<=0||hp3_len<=0)
 		{
-			alert("전화번호를 입력해주세요.")
-			join.hp2.focus()
-		}
-		
-		else if(!hpReg3.test(hp3_len))
-		{
-			alert("전화번호를 입력해주세요.")
+			alert("전화번호를 입력하세요.")
 			join.hp2.focus()
 		}
 
 		
 		else if(em1_len<=0||em2_len<=0)
 		{
-			alert("이메일주소를 입력해주세요.")
+			alert("이메일주소를 입력하세요.")
 			join.em1.focus()
 		}
 		
