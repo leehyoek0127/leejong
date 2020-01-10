b6f4fd71cba17b6a95efc1af948af391ea5bb245
leehyoek0127 <58802091+leehyoek0127@users.noreply.github.com>
2020-01-10 10:16:31 +0900

    sd

---
+++ src/YonglistDto.java
@@ -0,0 +1,78 @@
+import java.sql.Timestamp;
+
+public class YonglistDto {
+	String yMember;
+	Timestamp yDate;
+	String yBranch;
+	String yGround;
+	String yTel;
+	String yMatchdate;
+	String yTime;
+	String yApply;
+	String yTeam;
+	String yContents;
+	public String getyMember() {
+		return yMember;
+	}
+	public void setyMember(String yMember) {
+		this.yMember = yMember;
+	}
+	public Timestamp getyDate() {
+		return yDate;
+	}
+	public void setyDate(Timestamp yDate) {
+		this.yDate = yDate;
+	}
+	public String getyBranch() {
+		return yBranch;
+	}
+	public void setyBranch(String yBranch) {
+		this.yBranch = yBranch;
+	}
+	public String getyGround() {
+		return yGround;
+	}
+	public void setyGround(String yGround) {
+		this.yGround = yGround;
+	}
+	public String getyTel() {
+		return yTel;
+	}
+	public void setyTel(String yTel) {
+		this.yTel = yTel;
+	}
+	public String getyMatchdate() {
+		return yMatchdate;
+	}
+	public void setyMatchdate(String yMatchdate) {
+		this.yMatchdate = yMatchdate;
+	}
+	public String getyTime() {
+		return yTime;
+	}
+	public void setyTime(String yTime) {
+		this.yTime = yTime;
+	}
+	public String getyApply() {
+		return yApply;
+	}
+	public void setyApply(String yApply) {
+		this.yApply = yApply;
+	}
+	public String getyTeam() {
+		return yTeam;
+	}
+	public void setyTeam(String yTeam) {
+		this.yTeam = yTeam;
+	}
+	public String getyContents() {
+		return yContents;
+	}
+	public void setyContents(String yContents) {
+		this.yContents = yContents;
+	}
+
+	
+	
+	
+}
